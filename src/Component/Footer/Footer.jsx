25fdc37c87dee1bc09aa9a55f70a582f5747534f
Kabi krishna <124856639+kabi-60@users.noreply.github.com>
2024-04-26 00:01:06 -0700

    Update

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
    return (
-      <footer class="container m-4 mx-auto rounded-lg bg-white shadow-lg">
+      <footer class="container mx-auto rounded-lg bg-white shadow-lg">
          <div class="mx-auto w-full max-w-screen-xl p-4 md:py-8">
             <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8 dark:border-gray-700 " />
             <div class="flex items-center justify-between">
